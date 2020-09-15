efbd23e6f6d8cf3bd1f7711d2b70a2ffa34454d2
Alex Hurley <ahh1539@rit.edu>
2020-09-15 12:05:20 -0400

    app meant to mess around with email and stuff

---
+++ main.py
@@ -0,0 +1,44 @@
+import smtplib
+from getpass import getpass
+
+
+def login(email, password):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)  # Connect to the server
+        server.starttls()  # Use TLS
+        server.login(email, password)  # Login to the email server
+        return server
+    except smtplib.SMTPException:
+        print('login error')
+        exit()
+
+
+def send_email(recipient, num_emails_to_send, email, server):
+    while num_emails_to_send != 0:
+        try:
+            message = 'Subject: {}\n\n{}'.format("Hello from Python",
+                                                 "I wrote this app to destroy you, this is the {}th email".format(
+                                                     num_emails_to_send))
+            server.sendmail(email, recipient, message)  # Send the email
+            print('Success: Sent to {}'.format(recipient))
+        except smtplib.SMTPException:
+            print("Error: unable to send email")
+        num_emails_to_send = num_emails_to_send - 1
+
+
+if __name__ == '__main__':
+
+    username = input('\n'+"Username: ")
+    pword = getpass()
+    send_to = input("Who do you want to email: ")
+    if '@' not in send_to:
+        print("Invalid address!")
+        exit()
+    num_emails = int(input("How many times do you want to email: "))
+    if num_emails > 4:
+        print("That's too many emails!")
+        exit()
+    print("================================================" + "\n")
+    email_server = login(username, pword)
+    send_email(send_to, num_emails, username, email_server)
+    email_server.quit()  # Logout of the email server
